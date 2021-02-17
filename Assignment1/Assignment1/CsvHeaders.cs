0218af28e721b1baa0cd5243fb5037538d04b906
AditiDhage <Aditi.Dhage@smu.ca>
2021-02-17 18:29:06 -0400

    Updated header logic

--- Assignment1/Assignment1/CsvHeaders.cs
+++ Assignment1/Assignment1/CsvHeaders.cs
@@ -1,5 +1,4 @@
 ﻿using CsvHelper.Configuration;
-using System;
 using System.Text.RegularExpressions;
 
 namespace Assignment1.ClassFiles
@@ -36,134 +35,114 @@ namespace Assignment1.ClassFiles
         public static string dateOfFileNameBeingProcessed = "";
     }
 
-    public sealed class CsvHeadersMap : ClassMap<CsvHeaders>
+    public sealed class CsvHeadersMap : ClassMap <CsvHeaders>
     {
         public CsvHeadersMap()
         {
-            Map(m => m.Firstname).Name("First Name")
-                .TypeConverterOption.NullValues(string.Empty)
-                .Validate(field => {
-                    if (string.IsNullOrEmpty(field.ToString()))
-                    {
-                        string errorMsg = ("First Name is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
-                        Logger.Log($"An exception occurred in validating First Name:\nMessage: {errorMsg}");
-                        return false;
-                    }
-                    else return true;
-                });
+            Map(m => m.Firstname).Name("First Name").Validate(field => {
+                if (string.IsNullOrEmpty(field))
+                {
+                    string errorMsg = ("First Name is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
+                    Logger.Log($"An exception occurred in validating First Name:\nMessage: {errorMsg}");
+                    return false;
+                }
+                else return true;
+            });
 
 
-            Map(m => m.Lastname).Name("Last Name")
-                .TypeConverterOption.NullValues(string.Empty)
-                .Validate(field => {
-                    if (string.IsNullOrEmpty(field.ToString()))
-                    {
-                        string errorMsg = ("Last Name is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
-                        Logger.Log($"An exception occurred in validating Last Name:\nMessage: {errorMsg}");
-                        return false;
-                    }
-                    else return true;
-                });
+            Map(m => m.Lastname).Name("Last Name").Validate(field => {
+                if (string.IsNullOrEmpty(field))
+                {
+                    string errorMsg = ("Last Name is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
+                    Logger.Log($"An exception occurred in validating Last Name:\nMessage: {errorMsg}");
+                    return false;
+                }
+                else return true;
+            });
 
-            Map(m => m.Streetnumber).Name("Street Number")
-                .TypeConverterOption.NullValues(string.Empty)
-                .Validate(field => {
-                    if (string.IsNullOrEmpty(field.ToString()))
-                    {
-                        string errorMsg = ("Street Number is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
-                        Logger.Log($"An exception occurred in validating Street Number:\nMessage: {errorMsg}");
-                        return false;
-                    }
-                    else
-                        return true;
-                });
+            Map(m => m.Streetnumber).Name("Street Number").Validate(field => {
+                if (string.IsNullOrEmpty(field))
+                {
+                    string errorMsg = ("Street Number is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
+                    Logger.Log($"An exception occurred in validating Street Number:\nMessage: {errorMsg}");
+                    return false;
+                }
+                else
+                    return true;
+            });
 
-            Map(m => m.Street).Name("Street")
-                .TypeConverterOption.NullValues(string.Empty)
-                .Validate(field => {
-                    if (string.IsNullOrEmpty(field.ToString()))
-                    {
-                        string errorMsg = ("Street name is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + "");
-                        Logger.Log($"An exception occurred in validating Street:\nMessage: {errorMsg}");
-                        return false;
-                    }
-                    else
-                        return true;
-                });
+            Map(m => m.Street).Name("Street").Validate(field => {
+                if (string.IsNullOrEmpty(field))
+                {
+                    string errorMsg = ("Street name is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + "");
+                    Logger.Log($"An exception occurred in validating Street:\nMessage: {errorMsg}");
+                    return false;
+                }
+                else
+                    return true;
+            });
 
 
-            Map(m => m.City).Name("City")
-                .TypeConverterOption.NullValues(string.Empty)
-                .Validate(field => {
-                    if (string.IsNullOrEmpty(field.ToString()))//|| validalphanumericwithsplchars(field.ToString()))
-                    {
-                        string errorMsg = ("City is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
-                        Logger.Log($"An exception occurred in validating City:\nMessage: {errorMsg}");
-                        return false;
-                    }
-                    else return true;
-                });
+            Map(m => m.City).Name("City").Validate(field => {
+                if (string.IsNullOrEmpty(field) )//|| validalphanumericwithsplchars(field))
+                {
+                    string errorMsg = ("City is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
+                    Logger.Log($"An exception occurred in validating City:\nMessage: {errorMsg}");
+                    return false;
+                }
+                else return true;
+            });
 
-            Map(m => m.Province).Name("Province")
-                .TypeConverterOption.NullValues(string.Empty)
-                .Validate(field => {
-                    if (string.IsNullOrEmpty(field.ToString()))
-                    {
-                        string errorMsg = ("Province is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
-                        Logger.Log($"An exception occurred in validating Province:\nMessage: {errorMsg}");
-                        return false;
-                    }
-                    else return true;
-                });
+            Map(m => m.Province).Name("Province").Validate(field => {
+                if (string.IsNullOrEmpty(field))
+                {
+                    string errorMsg = ("Province is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
+                    Logger.Log($"An exception occurred in validating Province:\nMessage: {errorMsg}");
+                    return false;
+                }
+                else return true;
+            });
 
-            Map(m => m.Country).Name("Country")
-                .TypeConverterOption.NullValues(string.Empty)
-                .Validate(field => {
-                    if (string.IsNullOrEmpty(field.ToString()))
-                    {
-                        string errorMsg = ("Country is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
-                        Logger.Log($"An exception occurred in validating Country:\nMessage: {errorMsg}");
-                        return false;
-                    }
-                    else
-                        return true;
-                });
+            Map(m => m.Country).Name("Country").Validate(field => {
+                if (string.IsNullOrEmpty(field))
+                {
+                    string errorMsg = ("Country is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
+                    Logger.Log($"An exception occurred in validating Country:\nMessage: {errorMsg}");
+                    return false;
+                }
+                else
+                    return true;
+            });
 
-            Map(m => m.Postalcode).Name("Postal Code")
-                .TypeConverterOption.NullValues(string.Empty)
-                .Validate(field => {
-                    if (string.IsNullOrEmpty(field.ToString()))
-                    {
-                        string errorMsg = ("Postal code is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
-                        Logger.Log($"An exception occurred in validating Postal code:\nMessage: {errorMsg}");
-                        return false;
-                    }
-                    else return true;
-                });
+            Map(m => m.Postalcode).Name("Postal Code").Validate(field => {
+                if (string.IsNullOrEmpty(field))
+                {
+                    string errorMsg = ("Postal code is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
+                    Logger.Log($"An exception occurred in validating Postal code:\nMessage: {errorMsg}");
+                    return false;
+                }
+                else return true;
+            });
 
-            Map(m => m.Phonenumber).Name("Phone Number")
-                .TypeConverterOption.NullValues(string.Empty)
-                .Validate(field => {
-                    if (string.IsNullOrEmpty(field.ToString()))
-                    {
-                        string errorMsg = ("Phone Number is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
-                        Logger.Log($"An exception occurred in validating Phone Number:\nMessage: {errorMsg}");
-                        return false;
-                    }
-                    else return true;
-                });
+            Map(m => m.Phonenumber).Name("Phone Number").Validate(field => {
+                if (string.IsNullOrEmpty(field))
+                {
+                    string errorMsg = ("Phone Number is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
+                    Logger.Log($"An exception occurred in validating Phone Number:\nMessage: {errorMsg}");
+                    return false;
+                }
+                else return true;
+            });
 
-            Map(m => m.Email).Name("email Address")
-                .TypeConverterOption.NullValues(string.Empty)
-                .Validate(field => {
-                    if (field.ToString().Contains("null"))
-                    {
-                        string errorMsg = ("email Address is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
-                        Logger.Log($"An exception occurred in validating email Address:\nMessage: {errorMsg}");
-                        return false;
-                    }
-                    else return true;
-                });
+            Map(m => m.Email).Name("email Address").Validate(field => {
+                if (field.Contains("null"))
+                {
+                    string errorMsg = ("email Address is null in file:\n  " + CsvHeadersHelpers.currentFileNameBeingProcessed + " \nat row : " + CsvHeadersHelpers.currentRowBeingProcessed + ".");
+                    Logger.Log($"An exception occurred in validating email Address:\nMessage: {errorMsg}");
+                    return false;
+                }
+                else return true;
+            });
 
         }
 
